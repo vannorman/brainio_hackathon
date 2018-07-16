623553c884bf65dc0ec221c5e772dd53fbf6fa07
Jonathan Na <jna6@hawaii.edu>
2018-07-15 18:50:10 -0800

    Made Network Data UI
    Made Network Data UI in top left corner

---
+++ Assets/ChannelText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChannelText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Data information");
+        print("Channel 1: ");
+        print("Channel 2: ");
+        print("Channel 3: ");
+        print("Channel 4: ");
+        print("Channel 5: ");
+        print("Channel 6: ");
+        print("Channel 7: ");
+        print("Channel 8: ");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
